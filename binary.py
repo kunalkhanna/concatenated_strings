ac7a6ee929ddd92c8079019c96dc9a5e94d12867
kunalk7 <kunalk7@hotmail.com>
2016-02-25 12:34:48 -0800

    improve corner case handling

--- binary.py
+++ binary.py
@@ -5,8 +5,8 @@ import time
 from datetime import timedelta
 
 start_time = time.monotonic()
-#input_file = "words for problem.txt"
-input_file = "words_test.txt"
+input_file = "words for problem.txt"
+#input_file = "words_test.txt"
 
 with open(input_file) as cache:
     lines = [line.rstrip('\n') for line in cache]
@@ -14,12 +14,12 @@ print("Time to read file and store in list: %s", timedelta(seconds=time.monotoni
 
 
 def find_cat_str_binary(lines):
-    outfile = open('outfile.txt', 'w')
+    outfile = open('outfile_find_cat_str_binary.txt', 'w')
     # outfile.write("binary_search_implementation")
     database = list()
     idx = 0
     for item1 in lines:
-        sys.stdout.write("\r[---%d%%---]" % ((idx / len(lines)) * 100))  # Progress Bar
+        sys.stdout.write("\r[---%d%%---]" % (((idx + 1) / len(lines)) * 100))  # Progress Bar
         sys.stdout.flush()
         # outfile.write("\nitem1: "+item1)
         for item2 in islice(lines[idx + 1:], bisect.bisect_left(lines[idx:], item1), None):  # item2 where
@@ -37,85 +37,97 @@ def find_cat_str_binary(lines):
                         # outfile.write("\nitem3:"+item3)
                         # outfile.write("\nitem3:"+item3+"compare_str:"+item2[len(item1):len(item3)+len(item1)])
                         if item2.startswith(item3, len(item1), len(item3) + len(item1)):
-                            #outfile.write("\n" + item2)
+                            outfile.write("\n" + item2)
                             # if item2[len(item1):len(item3)+len(item1)] == item3:
                             database.append(item2)
                             break
 
 
                 else:
-                    #print("item1 in database:", item1)
+                    # print("item1 in database:", item1)
                     # outfile.write("\nitem2: "+item2 + " item1: "+item1)
                     continue
             else:
-                #print("item2 in database:", item2)
+                # print("item2 in database:", item2)
                 continue
         idx += 1
 
     outfile.close()
-    print(database)
+    # print(database)
     database = list(set(database))
     return database
 
 
 def find_cat_strings(lines):
+    outfile = open('outfile_find_cat_strings.txt', 'w')
     index = 0
     database = list()
     for item1 in lines:
-        sys.stdout.write("\r[--%d%%--]" % ((index / len(lines)) * 100))  # Progress Bar
-        sys.stdout.flush()
-        #print("item1:", item1)
-        matching = [items for items in lines if items.startswith(item1, 0, len(item1))]
-        #print("matching:", matching)
-        for item2 in matching:
-            #print("item2(possible str):", item2)
-            if item1 == item2:
-                continue
-                #print("replica:", item2)
-            else:
-                #                match = [items for items in lines if item2[len(item1)-1:len(items)+len(item1)] == items]
-                #print ("match with", item2[len(item1)])
-                match = [items for items in lines if items.startswith(item2[len(item1)])]
-                #print("match:", match)
-                for item3 in lines:
-                    if item3.startswith(item2[len(item1)]):
-                        #print("item3: ", item3, "compare_str:", item2[len(item1):len(item3) + len(item1)])
-                        if len(item3) <= len(item2[len(item1):len(item3) + len(item1)]):
-                            if item2[len(item1):len(item3) + len(
-                                    item1)] == item3:  # check only for list items matching the starting letter of remaining string
-                                #print("\nstring of words found: %s ", item2)
-                                check = [items for items in database if items==item2]
-                                #print ("check:",check)
-                                if check == item2:
+        if database.count(item1) > 0:
+            continue
+        elif len(item1) == 0:
+            # print ("null string:", item1)
+            continue
+        else:
+            sys.stdout.write("\r[--%d%%--]" % (((index + 1) / len(lines)) * 100))  # Progress Bar
+            sys.stdout.flush()
+            # print("item1:", item1,"index:" ,index, "len_of_lines", len(lines))
+            matching = [items for items in lines if items.startswith(item1, 0, len(item1))]
+            # print("matching:", matching)
+            for item2 in matching:
+                if database.count(item2) == 0:
+                    if item1 == item2:
+                        continue
+                        # print("replica:", item2)
+                    elif len(item2) == 0:
+                        # print ("null string:", item2)
+                        continue
+                    else:
+                        # print("item2(possible str):", item2)
+                        for item3 in lines:
+                            if item3.startswith(item2[len(item1)]):
+                                # print("item3: ", item3, "compare_str:", item2[len(item1):len(item3) + len(item1)])
+                                if len(item3) == 0:
+                                    # print ("null string:", item3)
                                     continue
-                                    #print ("item duplicate in database")
+                                elif len(item3) <= len(item2[len(item1):len(item3) + len(item1)]):
+                                    if item2[len(item1):len(item3) + len(
+                                            item1)] == item3:  # check only for list items matching the starting letter of remaining string
+                                        check = [items for items in database if items == item2]
+                                        # print ("check:",check)
+                                        if check == item2:
+                                            continue
+                                            # print ("item duplicate in database")
+                                        else:
+                                            # print("\nstring of words found: %s ", item2, "word exists in database:", database.count(item2))
+                                            database.append(item2)
+                                            outfile.write("\n" + item2)
+                                            # database = list(set(database))
+                                            break
+                                    else:
+                                        continue
+                                        # print ("Mismatch: item3:",item3, "compare_str:", item2[len(item1):len(item3)+len(item1)])
                                 else:
-                                    database.append(item2)
-                                    #database = list(set(database))
-                                    break
-                            else:
-                                continue
-                                # print ("Mismatch: item3:", item3, "compare_str:", item2[len(item1):len(item3)+len(item1)])
-                        else:
-                            continue
+                                    continue
         index += 1
     database = list(set(database))
-    #print("\n", database)
+    outfile.close()
+    # print("\n", database)
     return database
 
-
+'''
 start_time_bin = time.monotonic()
 strings = find_cat_str_binary(lines)
 exec_time_bin = timedelta(seconds=time.monotonic() - start_time_bin)
-print("Total execution time for find_cat_str_binary: %s", exec_time_bin)
+print("Total execution time for find_cat_strings: %s", exec_time_bin)
 strings.sort(key=len, reverse=True)
 print("Longest concatenated word:", strings[0])
 print("2nd Longest concatenated word:", strings[1])
-print("Total count of all concatenated words using binary:", len(strings))
+print("Total count of all concatenated words:", len(strings))
 
 # outfile = open('outfile.txt', 'w')
 # outfile.write("binary_search_implementation")
-
+'''
 start_time_non_bin = time.monotonic()
 strings = find_cat_strings(lines)
 exec_time_non_bin = timedelta(seconds=time.monotonic() - start_time_non_bin)
@@ -123,7 +135,9 @@ print("Total execution time for find_cat_strings: %s", exec_time_non_bin)
 strings.sort(key=len, reverse=True)
 print("Longest concatenated word:", strings[0])
 print("2nd Longest concatenated word:", strings[1])
-print("Total count of all concatenated words using non binary:", len(strings))
+print("Total count of all concatenated words:", len(strings))
 
-print("Performance benchmark: find_cat_str_binary is", ((exec_time_non_bin - exec_time_bin) / exec_time_bin) * 100,
-      "% faster than find_cat_strings")
+'''
+print("Performance benchmark: binary search is ", ((exec_time_non_bin - exec_time_bin) / exec_time_bin) * 100,
+      "% faster than non binary search")
+'''
